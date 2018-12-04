7f487461ed32c5562c3f62066cf4aecd2dbc537a
Iancu George-Alexandru <alex1gtp@gmail.com>
2018-12-04 16:23:51 +0200

    cookie session and serialize and deserialize user id

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,19 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/userModel');
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         // options for the strategy
@@ -11,13 +24,25 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) =>{
         
         // passport callback function
-        console.log(profile);
-        console.log('passport callback function fired');
-        new User({
-            username: profile.displayName,
-            googleID: profile.id
-        }).save().then((newUser) => {
-            console.log('new user created: ' + newUser);
-        });
+        // check if user already exists in DB
+        User.findOne({googleID: profile.id}).then((currentUser)=>{
+            if(currentUser){
+                //already have the user in DB
+                console.log('user is: ', currentUser);
+                // move to serializeUser <-
+                done(null, currentUser);
+            }
+            else{
+                //if not created in our DB -> create
+                new User({
+                    username: profile.displayName,
+                    googleID: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created: ' + newUser);
+                    // move to serializeUser <-
+                    done(null, newUser);
+                });
+            }
+        })
     })
 )
