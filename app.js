7ea08ad8b4bd42cec86e830a6feae5646a81a6b0
Iancu George-Alexandru <alex1gtp@gmail.com>
2018-12-04 15:45:50 +0200

    mongoDB on mLAB and store data

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 // set up view engine
 app.set('view engine', 'ejs')
+
+//connect to mongoDB
+mongoose.connect(keys.mongodb.dbURL, () => {
+    console.log('connected to mongoDB on mLAB');
+});
 
 //set up routes
 app.use('/auth',authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
-
+const User = require('../models/userModel');
 passport.use(
     new GoogleStrategy({
         // options for the strategy
         callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }, () =>{
+    }, (accessToken, refreshToken, profile, done) =>{
+        
         // passport callback function
+        console.log(profile);
+        console.log('passport callback function fired');
+        new User({
+            username: profile.displayName,
+            googleID: profile.id
+        }).save().then((newUser) => {
+            console.log('new user created: ' + newUser);
+        });
     })
 )
