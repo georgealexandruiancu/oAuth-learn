171911ad3c40393602948c5c06a81fb6a6a0fb08
Iancu George-Alexandru <alex1gtp@gmail.com>
2018-12-04 13:57:19 +0200

    setting up the routes and passport

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth-routes');
 
 // set up view engine
 app.set('view engine', 'ejs')
+
+//set up routes
+app.use('/auth',authRoutes);
 
 //app home route
 app.get('/', (req, res)=>{
